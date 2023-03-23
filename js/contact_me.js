9cc32784b931b319bdd285591b99cd5745933063
Matheus Toledo <matheus.toledo@mosaico.com.br>
2023-03-23 02:02:08 -0300

    changing url

--- js/contact_me.js
+++ js/contact_me.js
@@ -26,7 +26,7 @@ function submitForm(event) {
   };
 
   // Send the POST request with the message body using fetch()
-  fetch('https://public-ends-hunt-189-83-161-24.loca.lt/verly-leads-api/send', {
+  fetch('https://public-ends-hunt-189-83-161-24.loca.lt/verly-leads-api/save', {
     method: 'POST',
     headers: headers,
     body: JSON.stringify(postData)
