e59159030363825062118071158c0080c9a658ca
Matheus Toledo <matheus.toledo@mosaico.com.br>
2023-03-23 12:02:48 -0300

    changing url

--- js/contact_me.js
+++ js/contact_me.js
@@ -22,7 +22,7 @@ function submitForm(event) {
     };
 
   // Send the POST request with the message body using fetch()
-  fetch('https://itchy-pears-wonder-189-83-161-24.loca.lt/verly-leads-api/save', {
+  fetch('https://itchy-pears-wonder-189-83-161-24.loca.lt/verly-leads-api/send', {
     method: 'POST',
     headers: headers,
     body: JSON.stringify(postData)
